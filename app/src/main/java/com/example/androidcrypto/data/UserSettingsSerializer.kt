a9dec235c4f432f199b7b4e5cc0c90fe03cfc8a1
Alin Babu <alinbabu2010@outlook.com>
2023-05-26 13:36:10 +0530

    Add user settings serializer

---
+++ app/src/main/java/com/example/androidcrypto/data/UserSettingsSerializer.kt
@@ -0,0 +1,35 @@
+package com.example.androidcrypto.data
+
+import androidx.datastore.core.Serializer
+import com.example.androidcrypto.CryptoManager
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
+
+class UserSettingsSerializer(private val cryptoManager: CryptoManager) : Serializer<UserSettings> {
+
+    override val defaultValue: UserSettings
+        get() = UserSettings()
+
+    override suspend fun readFrom(input: InputStream): UserSettings {
+        val decryptedBytes = cryptoManager.decrypt(input)
+        return try {
+            Json.decodeFromString(
+                deserializer = UserSettings.serializer(),
+                string = decryptedBytes.decodeToString()
+            )
+        } catch (exception: SerializationException) {
+            exception.printStackTrace()
+            defaultValue
+        }
+    }
+
+    override suspend fun writeTo(t: UserSettings, output: OutputStream) {
+        cryptoManager.encrypt(
+            bytes = Json.encodeToString(serializer = UserSettings.serializer(), value = t)
+                .encodeToByteArray(),
+            outputStream = output
+        )
+    }
+}
