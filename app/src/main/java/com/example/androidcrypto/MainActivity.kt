970e4c4529798b4b317fbc4045c1a8c9be2526de
Alin Babu <alinbabu2010@outlook.com>
2023-05-26 12:23:15 +0530

    Add `EncryptDecryptView` composable in MainActivity
    Also add `CryptoManager` property

--- app/src/main/java/com/example/androidcrypto/MainActivity.kt
+++ app/src/main/java/com/example/androidcrypto/MainActivity.kt
@@ -3,15 +3,18 @@ package com.example.androidcrypto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.androidcrypto.components.EncryptDecryptView
 import com.example.androidcrypto.ui.theme.AndroidCryptoTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val cryptoManager = CryptoManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidCryptoTheme {
-
+                EncryptDecryptView(cryptoManager, filesDir)
             }
         }
     }
